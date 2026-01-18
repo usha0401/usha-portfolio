e457cd68eba0ac5cf649776e1c802d258ef5695d
Usha Sree Vasi <115167937+usha0401@users.noreply.github.com>
2026-01-18 22:09:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/usha-portfolio">
         <Routes>
           <Route index element={<Home /> } />
           <Route path="*" element={<NotFound />} />
